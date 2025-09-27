d69e2c403caa84a6a2fabc78400de5fc6fe6b3f0
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-09-26 22:09:05 -0300

    Correção de estilos do FaqItem

--- app/componentes/FaqItem/index.jsx
+++ app/componentes/FaqItem/index.jsx
@@ -8,8 +8,10 @@ function FaqItem({ titulo, children }) {
     return (
         <View style={estilos.accordionContainer}>
             <TouchableOpacity style={estilos.accordionHeader} onPress={() => setAberto(!aberto)}>
-                <Text style={estilos.accordionTitle}>{titulo}</Text>
-                <AntDesign name={aberto ? "caret-up" : "caret-down"} size={20} color={"#333"} />
+                <View style={estilos.headerContent}>
+                    <Text style={estilos.accordionTitle}>{titulo}</Text>
+                    <AntDesign name={aberto ? "caret-up" : "caret-down"} size={20} color={"#333"} />
+                </View>
             </TouchableOpacity>
             {aberto && <View style={estilos.accordionContent}>{children}</View>}
         </View>
@@ -19,8 +21,38 @@ function FaqItem({ titulo, children }) {
 export default FaqItem;
 
 const estilos = StyleSheet.create({
-    accordionContainer: { backgroundColor: '#FFF', borderRadius: 10, marginBottom: 15, overflow: 'hidden', borderWidth: 1, borderColor: '#E0E0E0' },
-    accordionHeader: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 15, backgroundColor: '#F9F9F9' },
-    accordionTitle: { fontSize: 16, fontWeight: 'bold', color: '#333' },
-    accordionContent: { paddingVertical: 10, paddingHorizontal: 20, borderTopWidth: 1, borderTopColor: '#E0E0E0' },
+    accordionContainer: {
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        marginBottom: 15,
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#E0E0E0'
+    },
+    accordionHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        backgroundColor: '#F9F9F9',
+    },
+    headerContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+    },
+    accordionTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333',
+        flexShrink: 1,
+        marginRight: 10,
+    },
+    accordionContent: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderTopWidth: 1,
+        borderTopColor: '#E0E0E0'
+    },
 });
